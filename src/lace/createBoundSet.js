c95af4cc0eddb2da47cc5a8b9655ce9ca815729f
André M. Arnold <andre.m.arnold@gmail.com>
2016-09-29 11:57:35 +0200

    add:resolve func dynamic on lace.

--- src/lace/createBoundSet.js
+++ src/lace/createBoundSet.js
@@ -1,7 +1,11 @@
 import lace_set from "./set"
 
 export default function lace_createBoundSet (fn)  {
-	return function lace_boundSet () {
-		return lace_set.call(this, fn)
-	}
+	return typeof(fn) === "string" || typeof(fn) === "symbol"
+	?	function lace_boundDynamSet () {
+			return lace_set.call(this, this[fn])
+		}
+	:	function lace_boundStaticSet () {
+			return lace_set.call(this, fn)
+		}
 }
