8465b9a1978e4755cdf111d4bd24480d57b5069f
André M. Arnold <andre.m.arnold@gmail.com>
2017-01-07 14:09:50 +0100

    fix:arguments length call/apply.

--- src/lace/apply.js
+++ src/lace/apply.js
@@ -1,6 +1,6 @@
 const apply_ = Function.prototype.apply
 
-export default function lace_apply () {
+export default function lace_apply (thisArg, argList) {
 	apply_.apply(this.fn, arguments)
 	return this
 }
--- src/lace/call.js
+++ src/lace/call.js
@@ -1,6 +1,6 @@
 const call_ = Function.prototype.call
 
-export default function lace_call () {
+export default function lace_call (thisArg) {
 	call_.apply(this.fn, arguments)
 	return this
 }
