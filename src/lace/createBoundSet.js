4d2c33030a7b53f7b72f2a32c89ae623dba73038
André M. Arnold <andre.m.arnold@gmail.com>
2017-01-07 13:26:52 +0100

    fix:boundSet not using context.

--- src/lace/createBoundSet.js
+++ src/lace/createBoundSet.js
@@ -3,7 +3,7 @@ import lace_set from "./set"
 export default function lace_createBoundSet (fn)  {
 	return typeof(fn) === "string" || typeof(fn) === "symbol"
 	?	function lace_boundDynamSet () {
-			return lace_set.call(this, this[fn])
+			return lace_set.call(this, this.ctx[fn])
 		}
 	:	function lace_boundStaticSet () {
 			return lace_set.call(this, fn)
